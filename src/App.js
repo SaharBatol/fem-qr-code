121ada7b950f3176847909aa64cd2f1c0db0841b
SaharBatol <sahar.batol@yahoo.co.uk>
2022-03-11 15:05:40 +0000

    complete qr code activity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.scss";
+import qr from "./images/image-qr-code.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outer-container">
+      <div className="qr-container">
+        <img src={qr} alt="QR-code" />
+        <h1>Improve your front-end skills by building projects</h1>
+        <h2>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </h2>
+      </div>
     </div>
   );
 }
